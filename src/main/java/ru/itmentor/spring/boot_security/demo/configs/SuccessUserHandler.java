2fc00b566783521e7a0a8d5882e06f9d7a848ef2
tebenkova100@icloud.com <tebenkova100@icloud.com>
2024-04-01 13:22:16 +0800

    Добавила решение

--- src/main/java/ru/itmentor/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -1,3 +1,4 @@
+
 package ru.itmentor.spring.boot_security.demo.configs;
 
 import org.springframework.security.core.Authentication;
@@ -17,7 +18,9 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/user");
+            httpServletResponse.sendRedirect("/user/user");}
+        else if (roles.contains("ROLE_ADMIN")) {
+            httpServletResponse.sendRedirect("/admin/users");
         } else {
             httpServletResponse.sendRedirect("/");
         }
